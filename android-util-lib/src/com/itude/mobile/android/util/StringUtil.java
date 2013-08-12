4d0381246d69bb9c0aeb57a129fc6fe3ad487f19
Coen Houtman <c.houtman@itude.com>
2013-08-12 15:24:23 +0200

    https://mobiledev.itude.com/jira/browse/MOBBL-847

--- android-util-lib/src/com/itude/mobile/android/util/StringUtil.java
+++ android-util-lib/src/com/itude/mobile/android/util/StringUtil.java
@@ -5,27 +5,19 @@ import java.text.CharacterIterator;
 import java.text.DecimalFormat;
 import java.text.DecimalFormatSymbols;
 import java.text.StringCharacterIterator;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.apache.commons.codec.binary.Hex;
 
-import android.text.Html;
-import android.text.InputType;
-import android.text.Spanned;
-import android.text.method.NumberKeyListener;
 import android.util.Log;
 
 public final class StringUtil
 {
-  private static final String      TAG   = "StringUtilities";
+  private static final String TAG   = "StringUtil";
 
-  private static NumberKeyListener _currencyNumberKeyListener;
-
-  public static final String       EMPTY = "";
+  public static final String  EMPTY = "";
 
   private StringUtil()
   {
@@ -61,97 +53,6 @@ public final class StringUtil
   public static String stripCharacter(String inputString, char stripCharacter)
   {
     return inputString.replaceAll(Pattern.quote(Character.toString(stripCharacter)), "");
-  }
-
-  /**
-   * Returns a List of path-parts with some light processing.
-   * for example the path
-   * /a/b/c/////.///d/../e
-   * is returned as a list containing
-   * a,b,c,e
-   * multiple adjacent /-es are ignored
-   * a . is removed
-   * a .. is interpreted as: pop the previous path part (d in the example above)  
-   * 
-   * @param path
-   * @return
-   */
-  public static List<String> splitPath(String toSplit)
-  {
-    // performance tuned implementation of splitPath
-    // measurements show this impl takes just 25% compared to the old
-    // implementation (splitPathOldImplementation which uses expensive Regular Expressions).
-    // note that the AndroidBinckTest project has a testcase that checks splitPath performance
-    // against splitPathOldImplementation performance. if splitPath doesn't perform at least
-    // 4 times as fast, the test fails.
-    List<String> components = new ArrayList<String>();
-    int previousPosition = 0;
-    int slashPosition;
-    while ((slashPosition = toSplit.indexOf('/', previousPosition)) >= 0)
-    {
-      String component = toSplit.substring(previousPosition, slashPosition);
-      previousPosition = slashPosition + 1;
-
-      processPathComponent(component, components, toSplit);
-    }
-    if (previousPosition < toSplit.length())
-    {
-      // this happens when the path is something like /a/b/c
-      // (no trailing forward slash).
-      String component = toSplit.substring(previousPosition);
-      processPathComponent(component, components, toSplit);
-    }
-    return components;
-  }
-
-  private static void processPathComponent(String component, List<String> componentsInPath, String completePath)
-  {
-    if (component.length() == 0 || (component.length() == 1 && component.equals(".")))
-    {
-      // nothing, ignore this component
-    }
-    else if (component.length() == 2 && component.equals(".."))
-    {
-      // pop the previous path component
-      if (componentsInPath.size() == 0)
-      {
-        //        throw new MBInvalidRelativePathException(completePath);
-        throw new ItudeException(completePath);
-      }
-      componentsInPath.remove(componentsInPath.size() - 1);
-    }
-    else
-    {
-      componentsInPath.add(component);
-    }
-  }
-
-  public static String normalizedPath(String path)
-  {
-    // try to prevent work in the normal case (the path is already normalized)
-    // especially the splitPath method-call is expensive.
-    if (path.indexOf('.') < 0 && path.indexOf("//") < 0)
-    {
-      // remove trailing / if present
-      if (path.endsWith("/")) return path.substring(0, path.length() - 1);
-      else return path;
-    }
-    boolean isRelative = !path.startsWith("/");
-
-    StringBuilder result = new StringBuilder();
-    for (String component : splitPath(path))
-    {
-      result.append('/').append(component);
-    }
-
-    if (isRelative && result.charAt(0) == '/')
-    {
-      return result.substring(1);
-    }
-    else
-    {
-      return result.toString();
-    }
   }
 
   /**
@@ -638,34 +539,5 @@ public final class StringUtil
     Pattern p = Pattern.compile(pattern);
     Matcher m = p.matcher(value);
     return m.matches() ? true : false;
-  }
-
-  public static Spanned fromHTML(String textToTransform)
-  {
-    return Html.fromHtml(textToTransform);
-  }
-
-  public static NumberKeyListener getCurrencyNumberKeyListener()
-  {
-    if (_currencyNumberKeyListener == null)
-    {
-      _currencyNumberKeyListener = new NumberKeyListener()
-      {
-
-        @Override
-        public int getInputType()
-        {
-          return InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD;
-        }
-
-        @Override
-        protected char[] getAcceptedChars()
-        {
-          return new char[]{'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ','};
-        }
-      };
-    }
-
-    return _currencyNumberKeyListener;
   }
 }
