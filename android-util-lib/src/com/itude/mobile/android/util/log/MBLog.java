f9ae4fe9e0d3d5583bd574cb997ed633d4730f80
w.elsinga@gmail.com <w.elsinga@gmail.com>
2014-11-13 15:31:41 +0100

    Cleaned up

--- android-util-lib/src/com/itude/mobile/android/util/log/MBLog.java
+++ android-util-lib/src/com/itude/mobile/android/util/log/MBLog.java
@@ -1,3 +1,18 @@
+/*
+ * (C) Copyright Itude Mobile B.V., The Netherlands
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.itude.mobile.android.util.log;
 
 import android.util.Log;
@@ -9,6 +24,7 @@ public class MBLog
 
   private static boolean shouldLog(int logLevel)
   {
+    // Not deadcode, only in IDE it is.
     if (BuildConfig.DEBUG || logLevel > Log.DEBUG)
     {
       /*
